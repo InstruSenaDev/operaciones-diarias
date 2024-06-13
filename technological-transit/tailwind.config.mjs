02acbc53013415a24f7eff51edb6addb44a2e566
Michel <checoaparicio9@gmail.com>
2024-06-13 11:48:03 -0500

    Avances en astro
    se cambio el lenguaje a astro y tailwind

---
+++ technological-transit/tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+	content: [
+	'./src/*/.{astro,html,js,jsx,md,mdx,svelve,ts,tsx,vue}',
+	],
+	theme: {
+	extend: {
+		colors: {
+		'Verde': '#A3E784',
+	},
+	},
+	variants: {},
+	plugins: [],
+}
+  }
